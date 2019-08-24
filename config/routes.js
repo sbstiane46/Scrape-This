a5aec8e228748ef0552e79e9a5a67bd990b19839
sbstiane46 <sbstian89@yahoo.com>
2019-08-24 18:29:33 -0400

    more

--- config/routes.js
+++ config/routes.js
@@ -5,11 +5,3 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 var exphbs = require("express-handlebars");
 var logger = require("morgan");
-
-//express initiation
-var app = express();
-app.use(logger("dev"));
-
-//router
-var router = express.Router();
-
